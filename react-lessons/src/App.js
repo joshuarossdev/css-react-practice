54ad3e46973ae14622e274f025162c731e8a9aa0
Joshua Ross <jjross.dev@gmail.com>
2020-09-03 18:23:49 -0700

    added dynamic render of ingredient data

--- react-lessons/src/App.js
+++ react-lessons/src/App.js
@@ -15,7 +15,19 @@ const sampleRecipes = [
     servings: 3,
     cookTime: '1:45',
     instructions: `1. Put salt on chicken\n2. Put
-      chicken in oven\n3. Eat chicken`
+      chicken in oven\n3. Eat chicken`,
+    ingredients: [
+      {
+        id: 1,
+        name: 'Chicken',
+        amount: '2 pounds'
+      },
+      {
+        id: 2,
+        name: 'Salt',
+        amount: '1 Tbs'
+      }
+    ]
   },
   {
     id: 2,
@@ -23,7 +35,19 @@ const sampleRecipes = [
     servings: 5,
     cookTime: '0:45',
     instructions: `1. Put paprika on pork\n2. Put
-      pork in oven\n3. Eat pork`
+      pork in oven\n3. Eat pork`,
+    ingredients: [
+      {
+        id: 1,
+        name: 'Pork',
+        amount: '3 pounds'
+      },
+      {
+        id: 2,
+        name: 'Paprika',
+        amount: '2 Tbs'
+      }
+    ]
   }
 ]
 
