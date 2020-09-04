60fbd5877e77be2239b4d5d521f5c1f96db55201
Joshua Ross <jjross.dev@gmail.com>
2020-09-03 17:44:58 -0700

    added recipe components and created dynamic recipe data rendering

--- react-lessons/src/App.js
+++ react-lessons/src/App.js
@@ -1,23 +1,30 @@
 import React, { useState } from 'react'
-import Counter from './Counter'
-import CounterHooks from './CounterHook'
+import RecipeList from './RecipeList';
 
-export const ThemeContext = React.createContext()
 
 function App() {
-  console.log ('render App')
-  const [theme, setTheme] = useState('red')
   return (
-    <ThemeContext.Provider value={{ backgroundColor: theme }}>
-      Counter
-      <Counter initialCount={0} />
-      CounterHooks
-      <CounterHooks initialCount={3} />
-      <button onClick={() => setTheme(prevTheme => {
-        return prevTheme === 'red' ? 'blue' : 'red'
-      })}>Toggle Theme</button>
-    </ThemeContext.Provider>
+    <RecipeList recipes={sampleRecipes}/>
   )
 }
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: 'Plain Chicken',
+    servings: 3,
+    cookTime: '1:45',
+    instructions: `1. Put salt on chicken\n2. Put
+      chicken in oven\n3. Eat chicken`
+  },
+  {
+    id: 2,
+    name: 'Plain Pork',
+    servings: 5,
+    cookTime: '0:45',
+    instructions: `1. Put paprika on pork\n2. Put
+      pork in oven\n3. Eat pork`
+  }
+]
 
 export default App;
