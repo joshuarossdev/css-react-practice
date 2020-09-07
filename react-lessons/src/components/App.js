213f7a771ef0e48785305e3022c13befafc30d39
Joshua Ross <jjross.dev@gmail.com>
2020-09-06 17:05:34 -0700

    added local storage for recipe data

--- react-lessons/src/components/App.js
+++ react-lessons/src/components/App.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react'
-import RecipeList from './RecipeList';
+import React, { useState, useEffect } from 'react'
+import RecipeList from './RecipeList'
+import RecipeEdit from './RecipeEdit'
 import '../css/app.css'
 import {v4 as uuidv4 } from 'uuid'
 
 export const RecipeContext = React.createContext()
+const LOCAL_STORAGE_KEY = 'cookingWithReact.recipes'
 
 function App() {
   const [recipes, setRecipes] = useState(sampleRecipes)
+
+  useEffect(() => {
+    const recipeJSON = localStorage.getItem(LOCAL_STORAGE_KEY)
+    if (recipeJSON != null) setRecipes(JSON.parse(recipeJSON))
+  }, [])
+
+
+  useEffect(() => {
+    console.log('Rendered');
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes))
+  }, [recipes])
+
 
   const recipeContextValue = {
     handleRecipeAdd,
@@ -34,6 +48,7 @@ function App() {
   return (
     <RecipeContext.Provider value={recipeContextValue}>
       <RecipeList recipes={recipes} />
+      <RecipeEdit />
     </RecipeContext.Provider>
   )
 }
---
+++ react-lessons/src/components/RecipeEdit.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react'
+
+export default function RecipeEdit() {
+  return (
+    <div className="recipe-edit">
+      <div>
+        <button>&times;</button>
+      </div>
+    </div>
+  )
+}
