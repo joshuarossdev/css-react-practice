53457e73c4416b16febef49a30135897f5d4ea65
Joshua Ross <jjross.dev@gmail.com>
2020-09-22 10:49:41 -0700

    Added edit recipe ingredient component

--- react-lessons/src/components/RecipeEdit.js
+++ react-lessons/src/components/RecipeEdit.js
@@ -1,10 +1,35 @@
 import React, {useState, useEffect} from 'react'
+import RecipeIngredient from './RecipeIngredientEdit'
+import RecipeIngredientEdit from './RecipeIngredientEdit'
 
 export default function RecipeEdit() {
   return (
     <div className="recipe-edit">
       <div>
         <button>&times;</button>
+      </div>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name"/>
+        <label htmlFor="Cook Time">Cook Time</label>
+        <input type="text" name="name" id="cookTime" />
+        <label htmlFor="servings">Servings</label>
+        <input type="number" min="1" name="servings"
+        id="servings" />
+        <label htmlFor="instructions">Instructions</label>
+        <textarea name="instrucitons" id="instructions" />
+      </div>
+      <br />
+      <label>Ingredients</label>
+      <div>
+        <div>Name</div>
+        <div>Amount</div>
+        <div></div>
+        <RecipeIngredientEdit />
+        <RecipeIngredientEdit />
+      </div>
+      <div>
+        <button>Add Ingredients</button>
       </div>
     </div>
   )
---
+++ react-lessons/src/components/RecipeIngredientEdit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function RecipeIngredientEdit() {
+  return (
+    <>
+      <input />
+      <input />
+      <button>&times;</button>
+    </>
+  )
+}
